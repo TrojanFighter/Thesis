5279fcc0987bb0882291d5f0dcf7021c561c1553
Ahmad Rushdi <aarushdi@gmail.com>
2016-07-18 00:18:50 -0600

    code updates

--- code/Optimal heding angle/paper/ode.m
+++ code/Optimal heding angle/paper/ode.m
@@ -8,9 +8,9 @@ Xc = (1-y(4))*cos(y(3)) + y(6)/y(1) * sin(y(3)) - y(5);
 xi = atan2(Xs,Xc);
 
 
-dydt = T*[ alpha*cos(phi) - cos(y(3))-xi;
--cos(xi)-cos(psi);
- -alpha/y(1) * sin(phi) + 1/y(1) * sin(y(3)-xi)- 1/y(2)*sin(psi) + 1/y(2) * sin(xi);
- y(6)/((y(1)^2))*(sin(y(3)-xi)-alpha*sin(phi));
- y(6)/((y(2)^2))*(sin(xi)-sin(psi));
- (1-y(4))*sin(y(3)-xi) - y(6)/y(1)*cos(y(3)-xi)];
+dydt = T*[ alpha*cos(phi) - cos(y(3)-xi);
+    -cos(xi)-cos(psi);
+    -alpha/y(1) * sin(phi) + 1/y(1) * sin(y(3)-xi)- 1/y(2)*sin(psi) + 1/y(2) * sin(xi);
+    y(6)/((y(1)^2)) * (sin(y(3)-xi)-alpha * sin(phi));
+    y(6)/((y(2)^2)) * (sin(xi) - sin(psi));
+    (1-y(4)) * sin(y(3)-xi) - y(6)/y(1)*cos(y(3)-xi)];
--- code/Optimal heding angle/paper/test.m
+++ code/Optimal heding angle/paper/test.m
@@ -1,7 +1,22 @@
-solinit = bvpinit(linspace(0,1),[2;3;1;2;1;2],6);
+clc; close all; clear; format short g
 
+solinit = bvpinit(linspace(0,1,5),[4;10;0.9;0.45;0.25;0.25],6);
+
+%options = bvpset('RelTol',0.1);
+%options = bvpset('NMax', 1E4);
 sol = bvp4c(@ode, @bc, solinit);
+
 y = sol.y;
-time = y(7)*sol.x;
-%ut = -y(4,:);
-plot(time,y(1,:),time,y(2,:),time,y(3,:)); %states vs time
+x = y(7) * sol.x;
+
+%ut = -y(4,:)
+
+% ===========
+h = figure('position',[100, 100, 500, 750]); set(gcf,'color','w'); set(gca,'FontSize',24);
+plot(x,y(1,:),'-b',x,y(2,:),'-k',x,y(3,:),'-r','LineWidth',3); 
+grid on
+h_legend = legend('[y_1]: R','[y_2]: r','[y_3]: \theta','Location','NorthEast');
+set(h_legend,'FontSize',14); set(gca,'FontSize',14);
+
+xlabel('Time [sec]','FontSize', 16); ylabel('States','FontSize', 16)
+saveTightFigure(h,'temp.png')
