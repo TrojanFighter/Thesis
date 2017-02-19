ab0d07d7ddf2d7020ab7f9dc3d404fe55c125bd4
Ahmad Rushdi <aarushdi@gmail.com>
2017-02-18 23:40:35 -0700

    code updates

---
+++ code/Optimal heding angle/paper/fig10_analytical.m
@@ -0,0 +1,71 @@
+clc; close all; clear; format short g
+% ===========
+%
+% Code to replicate figures from:
+% "Active Target Defense Differential Game" - Pachter, Garcia, Casbeer 
+% Allerton 2014
+%
+% ===========
+% Figure 8 
+
+alpha = 0.99; 
+beta = 1;
+
+VA = 1; 
+VT = alpha * VA; 
+VD = beta * VA;
+
+xA = 8;     yA = 0;
+xD = -8;    yD = 0;
+xT = 5; yT = 2;
+
+phi_hat = 2.5709;
+psi_hat = 0.7115;
+xi_hat  = 2.5643;
+
+%syms y
+c4 = 1 - alpha^2;
+c3 = -2*(1 - alpha^2) * yT;
+c2 = (1 - alpha^2)*yT^2 + xA^2 - alpha^2*xT^2;
+c1 = -2 * xA^2 * yT;
+c0 = xA^2 * yT^2;
+
+y = roots([c4 c3 c2 c1 c0])
+%solve( c4 * y^4 + c3 * y^3 + c2 * y^2 + c1 * y + c0 == 0, y)
+
+tf = (abs(xD)/cos(psi_hat)) / (VD)
+
+alpha_bar = (sqrt((xA+xT)^2+(yT^2)) - sqrt((xA-xT)^2+(yT^2)))/(2*xA)
+
+Target_distance = VT * tf
+
+xT = [xT xT + Target_distance*cos(phi_hat)]; 
+xA = [xA 0];
+xD = [xD 0];
+
+yT = [yT yT + Target_distance*sin(phi_hat)];
+yA = [yA real(y(3))];
+yD = [yD real(y(3))];
+
+h = figure('position',[100, 100, 1000, 750]); set(gcf,'color','w'); set(gca,'FontSize',24);
+
+plot(xT,yT,'-b','lineWidth',3); hold on
+plot(xA,yA,'-r','lineWidth',3)
+plot(xD,yD,'-k','lineWidth',3)
+
+plot(xT(1),yT(1),'-ob','MarkerSize',12,'MarkerFaceColor','b'); hold on
+plot(xA(1),yA(1),'-or','MarkerSize',12,'MarkerFaceColor','r'); 
+plot(xD(1),yD(1),'-ok','MarkerSize',12,'MarkerFaceColor','k');
+
+txt1 = 'T'; txt2 = 'A'; txt3 = 'D';
+tx1 = text(xT(1)+0.2,yT(1)+0.2,txt1); tx1.FontSize = 20;
+tx2 = text(xA(1)-0.2,yA(1)-0.2,txt2); tx2.FontSize = 20;
+tx3 = text(xD(1)+0.2,yD(1)-0.2,txt3); tx3.FontSize = 20;
+
+grid on
+h_legend = legend('Target','Attacker','Defender','Location','NorthEast');
+set(h_legend,'FontSize',14); set(gca,'FontSize',14);
+xlabel('x','FontSize', 16); ylabel('y','FontSize', 16)
+axis([-8 8 -1 8])
+saveTightFigure(h,'fig10.png')
+% ===========
--- code/Optimal heding angle/paper/fig_7ab_8_9_10_numerical.m
+++ code/Optimal heding angle/paper/fig_7ab_8_9_10_numerical.m
@@ -6,7 +6,11 @@ clc; close all; clear; format short g
 % Allerton 2014
 %
 % ===========
-solinit = bvpinit(linspace(0,1.75,3),[4;10;0.9;0.45;0.25;0.25],6);
+% WORKING COMBINATION: yinit guesses unjustified
+%solinit = bvpinit(linspace(0,1.75,3),[4; 10; 0.9; 0.45; 0.25; 0.25], 6);
+%sol = bvp4c(@ode, @bc, solinit);
+
+solinit = bvpinit(linspace(0,0.1,10),[4; 10; 0.9; 0.04; 0.02; 0.001], 6);
 sol = bvp4c(@ode, @bc, solinit);
 
 % ===========
@@ -42,9 +46,15 @@ set(h_legend,'FontSize',14); set(gca,'FontSize',14);
 xlabel('Time [sec]','FontSize', 16); ylabel('States','FontSize', 16)
 saveTightFigure(h,'temp.png')
 
-% =========== % Figure 8 
-alpha = 0.65; beta = 1;
-VA = 1; VT = alpha * VA; VD = beta * VA;
+% =========== 
+% Figure 8 
+
+alpha = 0.65; 
+beta = 1;
+
+VA = 1; 
+VT = alpha * VA; 
+VD = beta * VA;
 
 xA(1) = 5;     yA(1) = 0;
 xD(1) = -5;    yD(1) = 0;
@@ -83,7 +93,7 @@ plot(xA(1),yA(1),'-or','MarkerSize',12,'MarkerFaceColor','r');
 plot(xD(1),yD(1),'-ok','MarkerSize',12,'MarkerFaceColor','k'); 
 plot(xT,yT,'-b',xA,yA,'-r',xD,yD,'-k','LineWidth',3); 
 grid on
-h_legend = legend('Target','Attacker','Defender','Location','North');
+h_legend = legend('Target','Attacker','Defender','Location','NorthEast');
 set(h_legend,'FontSize',14); set(gca,'FontSize',14);
 xlabel('x','FontSize', 16); ylabel('y','FontSize', 16)
 %axis([-8 8 -2 8])
